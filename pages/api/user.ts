27cab450084b1bdd512d6942d74cbef5bb4ddedd
malexsan <alex@dotts.ro>
2021-02-25 13:46:19 +0200

    add command to start a docker container

--- pages/api/user.ts
+++ pages/api/user.ts
@@ -3,7 +3,5 @@ import { NextApiRequest, NextApiResponse } from 'next';
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
   const user = JSON.parse(req.body);
 
-  console.log('waiwai', user);
-
   res.status(200).json({ user });
 }
